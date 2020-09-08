7a0a77e3a7aa53a2f1a007388b790290a131f1d0
jcsanti <jean.charles.santi@gmail.com>
2020-09-08 20:00:25 +0200

    Adapt method to changes merged in rails' master branch
    1. 10b36e81a357f8d7fa3665630c4d41c057fe59d9
    We should initialize an "@joined_tables" hash which will later
    be used in "#make_constraints"
    
    2. 999bf3d0977edc48a18e943f24669dfe4bbdda54
    We should initialize an "@references" hash which will later be
    used in "#make_constraints" to handle table aliases

--- lib/polyamorous/activerecord_6.1_ruby_2/join_dependency.rb
+++ lib/polyamorous/activerecord_6.1_ruby_2/join_dependency.rb
@@ -1,1 +1,25 @@
 require 'polyamorous/activerecord_6.0_ruby_2/join_dependency'
+
+module Polyamorous
+  module JoinDependencyExtensions
+    def join_constraints(joins_to_add, alias_tracker, references)
+      @alias_tracker = alias_tracker
+      @joined_tables = {}
+      @references = {}
+
+      references.each do |table_name|
+        @references[table_name.to_sym] = table_name if table_name.is_a?(String)
+      end unless references.empty?
+
+      joins = make_join_constraints(join_root, join_type)
+
+      joins.concat joins_to_add.flat_map { |oj|
+        if join_root.match?(oj.join_root) && join_root.table.name == oj.join_root.table.name
+          walk(join_root, oj.join_root, oj.join_type)
+        else
+          make_join_constraints(oj.join_root, oj.join_type)
+        end
+      }
+    end
+  end
+end
