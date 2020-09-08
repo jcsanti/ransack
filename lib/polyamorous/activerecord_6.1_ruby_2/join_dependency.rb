00bd1aef15e038a6e0bcc7bec88cd96f956ec7ca
jcsanti <jean.charles.santi@gmail.com>
2020-09-08 20:49:35 +0200

    [tmp] Fix #make_constraints

--- lib/polyamorous/activerecord_6.1_ruby_2/join_dependency.rb
+++ lib/polyamorous/activerecord_6.1_ruby_2/join_dependency.rb
@@ -21,5 +21,33 @@ module Polyamorous
         end
       }
     end
+
+    private
+      def make_constraints(parent, child, join_type)
+        foreign_table = parent.table
+        foreign_klass = parent.base_klass
+
+        join_type = child.join_type || join_type if join_type == Arel::Nodes::InnerJoin
+
+        child.join_constraints(foreign_table, foreign_klass, join_type, alias_tracker) do |reflection|
+          table, terminated = @joined_tables[reflection]
+          root = reflection == child.reflection
+
+          if table && (!root || !terminated)
+            @joined_tables[reflection] = [table, root] if root
+            next table, true
+          end
+
+          table_name = @references[reflection.name.to_sym]
+
+          table = alias_tracker.aliased_table_for(reflection.klass.arel_table, table_name) do
+            name = reflection.alias_candidate(parent.table_name)
+            root ? name : "#{name}_join"
+          end
+
+          @joined_tables[reflection] ||= [table, root] if join_type == Arel::Nodes::OuterJoin
+          table
+        end.concat child.children.flat_map { |c| make_constraints(child, c, join_type) }
+      end
   end
 end
